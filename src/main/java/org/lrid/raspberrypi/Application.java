163b2476702dbe1993270f7b294ee5a5d4ec6523
lloydryandavid <lloydryan.david@gmail.com>
2018-12-25 14:37:54 +0800

    Initial commit

---
+++ src/main/java/org/lrid/raspberrypi/Application.java
@@ -0,0 +1,11 @@
+package org.lrid.raspberrypi;
+
+import org.springframework.boot.SpringApplication;
+
+public class Application {
+
+    public static void main(String[] args) {
+        
+    }
+
+}
